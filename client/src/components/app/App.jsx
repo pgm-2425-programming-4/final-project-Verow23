ad142c5103f80079af80b6eca85b940983e9947d
Verow23 <veronique.waroux@hotmail.com>
2025-05-19 16:34:33 +0200

    Page set-up

---
+++ client/src/components/app/App.jsx
@@ -0,0 +1,15 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import './App.css';
+import { PaginatedBacklog } from './paginated-backlog/paginated-backlog';
+
+const queryClient = new QueryClient()
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <PaginatedBacklog />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
