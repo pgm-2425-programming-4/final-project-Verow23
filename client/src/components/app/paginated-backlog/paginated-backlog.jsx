42821aa171dd792f6e0853703f19c3fac56c35f7
Verow23 <veronique.waroux@hotmail.com>
2025-05-19 16:36:27 +0200

    Added pagination to backlog

---
+++ client/src/components/app/paginated-backlog/paginated-backlog.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query"
+import { Pagination } from "./pagination/pagination";
+import { getTasks } from "../../../data/getTasks";
+import { Backlog } from "./backlog/Backlog";
+
+export function PaginatedBacklog() {
+    const [currentPage, setCurrentPage] = useState(1);
+    const [backlog, setBacklog] = useState([]);
+    const [pageCount, setPageCount] = useState(1)
+    const [pageSize, setPageSize] = useState(10)
+
+
+    function handlePageChanged(pageNumber) {
+        setCurrentPage(pageNumber);
+    }
+
+    function handleSizeChanged(pageSize) {
+        setPageSize(pageSize)
+    }
+
+    const { isPending, isError, error, data: responseData } = useQuery({
+        queryKey: ["backlog", { currentPage, pageSize }]
+        , queryFn: () => getTasks(currentPage, pageSize)
+    })
+
+    useEffect(() => {
+        if (responseData) {
+            if (currentPage > responseData.meta.pagination.pageCount) {
+                setCurrentPage(responseData.meta.pagination.pageCount)
+            }
+            setBacklog(responseData.data)
+            setPageCount(responseData.meta.pagination.pageCount)
+        }
+    }, [responseData, currentPage])
+
+    if (isPending) {
+        return <span>Loading...</span>
+    }
+
+    if (isError) {
+        return <span>Error: {error.message}</span>
+    }
+
+    console.log(backlog)
+
+    return (
+        <>
+            <Backlog backlog={backlog} />
+            <Pagination
+                currentPage={currentPage}
+                pageCount={pageCount}
+                pageSize={pageSize}
+                onPageChanged={handlePageChanged}
+                onSizeChanged={handleSizeChanged}
+            />
+        </>
+    );
+}
