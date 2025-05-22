5ea0f3cbe2a521f58e75f1f8fa3f2f6729b738e6
Verow23 <veronique.waroux@hotmail.com>
2025-05-22 11:23:41 +0200

    Added URL and token for online use

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,4 +1,10 @@
-export const API_URL = "http://localhost:3000/api";
-export const API_TOKEN =
-    "e687e04273c63d5c79241cb5422522c6770357c98aac293df1960da388f6eebb69d6702ff6538cd1f7cd04d3e924f6929a73ea1ac92226fb4b67d096de44103dcba058bfa119668c2f9b04cd635d3dbc7c3aee54c1e1863d39f3754ba255f5c2c3a87801d9d607ce8a193dce8baa8abaae5464cb5ddaf41034e11f563b65a067";
+export const API_URL = import.meta.env.PROD
+    ? "https://final-project-verow23.onrender.com/api"
+    : "http://localhost:3000/api";
+
+export const API_TOKEN = import.meta.env.PROD
+    ? "da4482765825d43fd7600e377d6b6fba1e7b5b706caf7ea7b9a630b7c79b1ae89cea49ab4132779131765ddf587d3598c13570bb240cb498fc87e19eeefe27bb45e4b06dff4d1849d914cb34fbf69ea9a9e0980bb20de7ae9ff9049038d2826519fba3a04ac94ec0d34a402e8b124be41ac50e7dcdf6d6966d25cd2c7ea76393"
+    : "fb165e03f8dec7d7e3abb37b6a997aa6f7ea8e101727b3c43a8780028115c65aea793dcca2a372a1437b279605335e03cefb9531d51c035c349e628aedc9fb21de40818e01ab3c69dd8d0d60a8d2168d620088211742c5079109ae5af355a5a3c64bc5df05f3af32abc8347b9aba7776ae65a1feee1e8a38bf8bd1c75de211e4";
+
+
 export const PAGE_SIZE_OPTIONS = [5, 10, 25, 50];
