38177da36383c1ee94409438d338b70ce30f5f6b
Verow23 <veronique.waroux@hotmail.com>
2025-06-10 14:49:26 +0200

    Fixed backlog page

--- client/src/components/app/board/Board.jsx
+++ client/src/components/app/board/Board.jsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import { getStatuses } from "../../../queries/getStatuses";
 import { Columns } from "./Columns";
+import { Link } from "@tanstack/react-router";
 
 export function Board({ tasks }) {
     const [statuses, setStatuses] = useState([])
@@ -30,7 +31,10 @@ export function Board({ tasks }) {
 
         <main className='board'>
             <h1>Active project {tasks[0].project.Title}</h1>
-            <Columns className='columns' statuses={statuses} tasks={tasks} />
+            <button className="backlog-button"><Link to="/projects/$projectSlug/backlog" params={{ projectSlug: tasks[0].project.slug }} >Backlog</Link></button>
+            <div className='columns' >
+                <Columns statuses={statuses} tasks={tasks} />
+            </div>
         </main>
 
     )
--- client/src/components/app/board/Columns.jsx
+++ client/src/components/app/board/Columns.jsx
@@ -1,13 +1,19 @@
 import { Cards } from "./Cards";
 
 export function Columns({ statuses, tasks }) {
+    console.log(statuses);
+
     return (<>
-        {statuses.map(status => (
-            <div key={status.id} className='column'>
-                <h3>{status.Title}</h3>
-                <Cards status={status} tasks={tasks} />
-            </div>
-        ))}
+        {statuses.map(status => {
+            if (status.Title !== "Backlog") {
+                return (
+                    <div key={status.id} className='column'>
+                        <h3>{status.Title}</h3>
+                        <Cards status={status} tasks={tasks} />
+                    </div>
+                )
+            }
+        })}
     </>
     )
 }
