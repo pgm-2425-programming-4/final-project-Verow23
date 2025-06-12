0386ce8f232438bf94335cb9debec4214668ec88
Verow23 <veronique.waroux@hotmail.com>
2025-06-12 10:16:45 +0200

    Added styling for modal

--- client/src/components/app/board/Board.jsx
+++ client/src/components/app/board/Board.jsx
@@ -67,7 +67,7 @@ export function Board({ tasks, projectSlug }) {
                     })}
                 </div>
                 {
-                    showModal && <TaskModal project={taskList[0].project} task={selectedTask} states={statuses} labels={labels} onClose={() => setShowModal(false)} onUpdate={refreshTasks} onDelete={refreshTasks} />
+                    showModal && <TaskModal className={(showModal) ? "is-active" : ""} project={taskList[0].project} task={selectedTask} states={statuses} labels={labels} onClose={() => setShowModal(false)} onUpdate={refreshTasks} onDelete={refreshTasks} />
                 }
             </main>
 
--- client/src/components/app/board/TaskModal.jsx
+++ client/src/components/app/board/TaskModal.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { API_TOKEN, API_URL } from "../../../constants/constants";
 
-export function TaskModal({ task, onUpdate, onDelete, states, onClose, labels, project }) {
+export function TaskModal({ task, onUpdate, onDelete, states, onClose, labels, project, className }) {
     const [form, setForm] = useState({
         title: task?.Title || "",
         description: task?.Description || "",
@@ -79,35 +79,45 @@ export function TaskModal({ task, onUpdate, onDelete, states, onClose, labels, p
     }
 
     return (
-        <div className="modal-container">
-            <div>
-                <p>{project.Title}</p>
-                <h2>{task ? "Edit task" : "New task"}</h2>
-                <input name="title" value={form.title} onChange={handleChange} placeholder="Title" />
-                <textarea name="description" value={form.description} onChange={handleChange} placeholder="Description"></textarea>
-                <select required name="state" value={form.state} onChange={handleChange}>
-                    <option disabled value="">Select your status</option>
-                    {states.map(state => (<option key={state.id} value={state.id}>{state.Title}</option>))}
-                </select>
-                <div>
-                    <label>Labels</label>
-                    <div>
-                        {labels.data.map(label => (<label key={label.id}>
-                            <input
-                                type="checkbox"
-                                checked={form.labels.includes(label.id)}
-                                onChange={() => toggleLabel(label.id)}
-                            />
-                            {label.attributes?.name || label.Title}
-                        </label>))}
-                    </div>
-                </div>
-                <div>
-                    {task?.id && <button onClick={handleDelete}>Delete</button>}
-                    <div>
-                        <button onClick={onClose} >Cancel</button>
-                        <button onClick={handleSubmit} >Save</button>
-                    </div>
+        <div className={`modal ${className}`} >
+            <div className="modal-background">
+                <div className="modal-card">
+                    <header className="modal-card-head">
+                        <p className="modal-card-title">{project.Title}</p>
+                    </header>
+                    <section className="modal-card-body">
+                        <h2>{task ? "Edit task" : "New task"}</h2>
+                        <div className="form">
+                            <input name="title" value={form.title} onChange={handleChange} placeholder="Title" />
+                            <textarea name="description" value={form.description} onChange={handleChange} placeholder="Description"></textarea>
+                            <select required name="state" value={form.state} onChange={handleChange}>
+                                <option disabled value="">Select your status</option>
+                                {states.map(state => (<option key={state.id} value={state.id}>{state.Title}</option>))}
+                            </select>
+                            <div>
+                                <label>Labels</label>
+                                <div>
+                                    {labels.data.map(label => (<label key={label.id}>
+                                        <input
+                                            type="checkbox"
+                                            checked={form.labels.includes(label.id)}
+                                            onChange={() => toggleLabel(label.id)}
+                                        />
+                                        {label.attributes?.name || label.Title}
+                                    </label>))}
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    <footer className="modal-card-foot">
+                        <div className="buttons">
+
+                            <button onClick={handleSubmit} className="button is-success" >Save</button>
+                            <button onClick={onClose} className="button" >Cancel</button>
+                            {task?.id && <button onClick={handleDelete} className="button is-danger">Delete</button>}
+
+                        </div>
+                    </footer>
                 </div>
             </div>
         </div>
