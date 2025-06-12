ee21118c9b7bde7d933e6211ad7906e49376f0e0
Verow23 <veronique.waroux@hotmail.com>
2025-06-12 18:38:41 +0200

    Added styling for labels on cards

--- client/src/components/app/board/Card.jsx
+++ client/src/components/app/board/Card.jsx
@@ -4,7 +4,7 @@ export function Card({ task, onClick }) {
         <div key={task.id} className='card is-clickable' onClick={onClick}>
             <p>{task.Title}</p>
             <div className='labels'>
-                {task.labels?.map((label, i) => (<span key={i} className='tag'>{label.Title}</span>))}
+                {task.labels?.map((label, i) => (<span key={i} className={label.Title === "Front-end" ? "tag is-primary is-light mx-1" : label.Title === "Back-end" ? "tag is-link is-light mx-1" : label.Title === "Urgent" ? "tag is-danger is-light mx-1" : "tag is-light mx-1"}>{label.Title}</span>))}
             </div>
         </div>
     </>)
