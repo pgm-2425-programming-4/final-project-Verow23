6cb4a42a1ecc8c7f7ed04b9793ee5477b3870a64
Verow23 <veronique.waroux@hotmail.com>
2025-06-12 17:12:28 +0200

    Added styling for board

--- client/src/components/app/board/Card.jsx
+++ client/src/components/app/board/Card.jsx
@@ -1,7 +1,7 @@
 export function Card({ task, onClick }) {
 
     return (<>
-        <div key={task.id} className='card' onClick={onClick}>
+        <div key={task.id} className='card is-clickable' onClick={onClick}>
             <p>{task.Title}</p>
             <div className='labels'>
                 {task.labels?.map((label, i) => (<span key={i} className='tag'>{label.Title}</span>))}
--- client/src/components/app/board/Column.jsx
+++ client/src/components/app/board/Column.jsx
@@ -3,9 +3,15 @@ import { Card } from "./Card";
 export function Column({ status, tasks, onTaskClick }) {
 
     return (
-        <div key={status.id} className='column'>
-            <h3>{status.Title}</h3>
-            {tasks.map(task => (<Card key={task.id} task={task} onClick={() => onTaskClick(task)} />))}
+        <div key={status.id} className='column is-on-quarter'>
+            <div className={status.Title === "To do" ? "message is-info" : status.Title === "In progress" ? "message is-danger" : status.Title === "Ready for Review" ? "message is-warning" : status.Title === "Done" ? "message is-success" : "message"}>
+                <div className="message-header">
+                    <h3>{status.Title}</h3>
+                </div>
+                <div className="message-body">
+                    {tasks.map(task => (<Card key={task.id} task={task} onClick={() => onTaskClick(task)} />))}
+                </div>
+            </div>
         </div>
     )
 }
